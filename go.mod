dbba6df06a477f2d59e9b34f12f8c91db357ef8e
Paul Farver <paulfarver@gmail.com>
2020-10-23 21:41:00 +0200

    WIP: github handler
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/uniwise/git-releaser
 go 1.15
 
 require (
+	github.com/Masterminds/semver/v3 v3.1.0
 	github.com/go-git/go-git v4.7.0+incompatible
 	github.com/google/go-github/v32 v32.1.0
 	github.com/joho/godotenv v1.3.0
--- pkg/githubclient.go
+++ pkg/githubclient.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 
+	semver "github.com/Masterminds/semver/v3"
 	"github.com/google/go-github/v32/github"
 	"golang.org/x/oauth2"
 )
@@ -45,14 +47,60 @@ func (c *ClientImpl) HandlePushEvent(ev *github.PushEvent) (interface{}, error) 
 	if pushed != master {
 		return nil, nil
 	}
-	fmt.Printf("Master branch pushed. Release scheduled...\n")
+	fmt.Printf("Master branch pushed. pre-release scheduled...\n")
+
+	tag, _, err := c.client.Repositories.GetLatestRelease(context.Background(), owner, repo)
+	if err != nil {
+		return nil, err
+	}
+	latest, err := semver.NewVersion(tag.GetTagName())
+	if err != nil {
+		return nil, err
+	}
+
+	comparison, _, err := c.client.Repositories.CompareCommits(context.Background(), owner, repo, tag.GetTargetCommitish(), ev.GetAfter())
+	if err != nil {
+		return nil, err
+	}
+	level := func() int {
+		lvl := 1
+		for _, commit := range comparison.Commits {
+			prs, _, err := c.client.PullRequests.ListPullRequestsWithCommit(commit)
+			if err != nil {
+				return nil, err
+			}
+			for _, pr := range prs {
+				for _, label := range pr.Labels {
+					if label.GetName() == "major" {
+						return 3
+					} else if label.GetName() == "minor" {
+						lvl = 2
+					}
+				}
+			}
+		}
+		return lvl
+	}()
+
+	var next semver.Version
+	switch level {
+	case 2:
+		next = latest.IncMinor()
+	case 3:
+		next = latest.IncMajor()
+	default:
+		next = latest.IncPatch()
+	}
+
+	con, err := semver.NewConstraint(fmt.Sprintf("> %s-rc.0 < %s", next.Original(), next.Original()))
+	next.String()
+	con.Check()
 
 	page := 0
 	tags := []*github.RepositoryTag{}
 	for {
 		nexttags, out, err := c.client.Repositories.ListTags(context.Background(), owner, repo, &github.ListOptions{
-			Page:    page,
-			PerPage: 1,
+			Page: page,
 		})
 		if err != nil {
 			return nil, err
@@ -64,10 +112,18 @@ func (c *ClientImpl) HandlePushEvent(ev *github.PushEvent) (interface{}, error) 
 		page = out.NextPage
 	}
 
+	versions := semver.Collection{}
 	for _, tag := range tags {
+		max := semver.NewVersion(latest.GetTagName())
 		fmt.Printf("tag found: %v", tag.GetName())
+		version
+		version, err := semver.NewVersion(tag.GetName())
+		if err != nil {
+			continue
+		}
+		versions = append(versions, version)
 	}
-
+	sort.Sort(versions)
 	// Find latest release
 
 	// Calculate new release version
