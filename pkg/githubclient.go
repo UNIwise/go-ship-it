f0cb40bc0661772c69b2c5b58ea263abb49149e5
Paul Farver <paulfarver@gmail.com>
2020-10-23 13:42:02 +0200

    WIP github release client
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- pkg/githubclient.go
+++ pkg/githubclient.go
@@ -2,10 +2,11 @@ package pkg
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/google/go-github/v32/github"
-	"github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 )
 
@@ -33,6 +34,69 @@ func NewClient() Client {
 }
 
 func (c *ClientImpl) HandlePushEvent(ev *github.PushEvent) (interface{}, error) {
-	logrus.Debugf("Handling push event to %v", ev.GetBaseRef())
+
+	fmt.Printf("Handling push event to %s on %s\n", ev.GetRepo().GetFullName(), ev.GetRef())
+
+	owner := ev.GetRepo().GetOwner().GetName()
+	repo := ev.GetRepo().GetName()
+	pushed := strings.TrimPrefix(ev.GetRef(), "refs/heads/")
+	master := ev.GetRepo().GetMasterBranch()
+
+	if pushed != master {
+		return nil, nil
+	}
+	fmt.Printf("Master branch pushed. Release scheduled...\n")
+
+	page := 0
+	tags := []*github.RepositoryTag{}
+	for {
+		nexttags, out, err := c.client.Repositories.ListTags(context.Background(), owner, repo, &github.ListOptions{
+			Page:    page,
+			PerPage: 1,
+		})
+		if err != nil {
+			return nil, err
+		}
+		tags = append(tags, nexttags...)
+		if out.NextPage == 0 {
+			break
+		}
+		page = out.NextPage
+	}
+
+	for _, tag := range tags {
+		fmt.Printf("tag found: %v", tag.GetName())
+	}
+
+	// Find latest release
+
+	// Calculate new release version
+
+	// Create new release
+
+	// repo := ev.GetRepo()
+	// repo.GetMasterBranch()
+
+	// ref := ev.GetRef()
+
+	// b, _, err := c.client.Repositories.GetBranch(context.Background(), repo.GetOwner().GetName(), repo.GetName(), repo.GetMasterBranch())
+
+	// // Check if new release should be created
+	// // repo := ev.GetRepo()
+
+	// // ref := ev.GetRef()
+	// // c.client.Repositories.GetBranch(context.Background(), repo.GetOwner().GetName(), repo.GetName(), )
+	// // r, _, err := c.client.Repositories.Get(context.Background(), repo.GetOwner().GetName(), repo.GetName())
+	// // reference, _, err := c.client.Git.GetRef(context.Background(), repo.GetOwner().GetName(), repo.GetName(), ref)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// c.client.Repositories.CreateRelease(context.Background(), repo.GetOwner().GetName(), repo.GetName(), &github.RepositoryRelease{
+	// 	Prerelease: github.Bool(true),
+	// 	Name:       github.String("2.18.5"),
+	// 	TagName:    github.String("v2.18.5"),
+	// })
+
 	return nil, nil
 }
